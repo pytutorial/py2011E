806a1b03115ff69291ccbcd198c3c7e7be95d9eb
tung.duong <duongthanhtungvn01@gmail.com>
2021-01-11 21:50:20 +0700

    Update

---
+++ Day11/vd6.py
@@ -0,0 +1,24 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from getpass import getpass
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "duongthanhtungvn01@gmail.com"
+receiver_email = "duongthanhtungvn01@gmail.com"
+
+password = getpass('Enter your password: ')
+
+message = """
+       Sample Email sent by Python.
+       """
+
+msg = MIMEText(message)
+msg['to'] = receiver_email
+msg['Subject'] = 'Python send mail test program'
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+   server.login(sender_email, password)
+   server.sendmail(sender_email, receiver_email, msg.as_string())
